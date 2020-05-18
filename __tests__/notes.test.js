4addcc4eecd9f96859d4cdb3ea854c843502fb4c
Tumaho <obadatumah19@gmail.com>
2020-05-19 00:42:54 +0300

    class02

---
+++ __tests__/notes.test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const Notes = require('../lib/notes');
+
+jest.spyOn(global.console, 'log');
+
+describe('execute()', () => {
+    it('nothing do when invoked invalid note', () => {
+        const creatNote = new Notes();
+        creatNote.execute();
+        expect(console.log).not.toHaveBeenCalled();
+    });
+
+    it('do when invoked valid note', () => {
+        const creatNote = new Notes();
+        creatNote.execute({ payload: 'my first test' });
+        expect(console.log).toHaveBeenCalled();
+    });
+});
+
+describe('add()', () => {
+
+    
+    it('logs the output message when add function is invoked with a valid note', () => {
+        const createNote = new Notes();
+        createNote.add({ payload: 'my first test' });
+        expect(console.log).toHaveBeenCalled();
+    });
+
+    it('Do nothing when add function is invoked with invalid note', () => {
+        const createNote = new Notes();
+        createNote.add();
+        expect(console.log).not.toHaveBeenCalled();
+    });
+
+});
